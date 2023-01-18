abe8cd203aab3e43d41c3007a8d9ef79e1ae687b
Jesus Santana-Roman <jesus.santana0320@gmail.com>
2023-01-18 07:19:26 -0400

    add firebase and first build

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,14 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/services.dart';
 import 'package:sonda/core/constants/styles.dart';
 import 'package:sonda/ui/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const Sonda(),
   );
